4121a2de663e34064d4e8d2c5a194d8ce855226a
Dimaceo18 <dimaceo@gmail.com>
2026-05-12 23:06:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ WHITE = (255, 255, 255)
 
 FONT_BOLD = "Montserrat-Black.ttf"
 FONT_REGULAR = "Montserrat-Bold.ttf"
-DIVIDER_PATH = "divider3.png"
+DIVIDER_PATH = "3322.png"
 
 # Высота плашки-разделителя в пикселях
 DIVIDER_HEIGHT = 50
